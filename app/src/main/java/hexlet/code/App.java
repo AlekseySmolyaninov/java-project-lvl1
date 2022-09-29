82f76ee562301beac755363748e37dc486bbb660
Aleksey Smolyaninov <s.32546875148@gmail.com>
2022-09-29 13:49:51 +0300

    deleted magic number

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,28 +7,34 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
+        int gameGreet = 1;
+        int gameEven = 2;
+        int gameCalc = 3;
+        int gameGCD = 4;
+        int gameProgression = 5;
+        int gamePrime = 6;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter.\n 1 - Greet\n 2 - Even\n 3 - Calc\n 4 - GCD\n " +
-                "5 - Progression\n 6 - Prime\n 0 - Exit\n " + "Your choice: ");
+        System.out.println("Please enter the game number and press Enter.\n 1 - Greet\n 2 - Even\n 3 - Calc\n "
+                + "4 - GCD\n " + "5 - Progression\n 6 - Prime\n 0 - Exit\n " + "Your choice: ");
         int gameNumber = sc.nextInt();
         if (gameNumber == 0) {
             return;
         }
         System.out.println("Welcome to the Brain Games!");
 
-        if (gameNumber == 2) {
-            Even.gameEven();
-        } else if (gameNumber == 3) {
-            Calc.gameCalc();
-        } else if (gameNumber == 4) {
-            GCD.gameGcd();
-        } else if (gameNumber == 5) {
-            Progression.gameProgression();
-        } else if (gameNumber == 6) {
-            Prime.gamePrime();
-        } else if (gameNumber == 1) {
+        if (gameNumber == gameGreet) {
             Greet.nameUser();
+        } else if (gameNumber == gameEven) {
+            Even.gameEven();
+        } else if (gameNumber == gameCalc) {
+            Calc.gameCalc();
+        } else if (gameNumber == gameGCD) {
+            GCD.gameGcd();
+        } else if (gameNumber == gameProgression) {
+            Progression.gameProgression();
+        } else if (gameNumber == gamePrime) {
+            Prime.gamePrime();
         }
     }
 }
+
