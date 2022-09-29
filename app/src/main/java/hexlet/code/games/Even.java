55f150f8d8ba89dfbe484f6fc655b4a02eae5048
Aleksey Smolyaninov <s.32546875148@gmail.com>
2022-09-29 22:55:37 +0300

    final10

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -3,14 +3,18 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import org.apache.commons.lang3.RandomUtils;
 
-import java.util.Scanner;
 
 public class Even {
     public static void gameEven() {
+
         String rule = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+
         String[][] questionAndAnswers = new String[Engine.rounds][2];
+
         for (var i = 0; i < Engine.rounds; i++) {
+
             int randomNumber = RandomUtils.nextInt(1, 1000);
+
             questionAndAnswers[i][0] = String.valueOf(randomNumber);
             questionAndAnswers[i][1] = isEven(randomNumber) ? "yes" : "no";
         }
