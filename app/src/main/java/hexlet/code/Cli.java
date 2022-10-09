f3a58c004f4904aec009df153dd8797623b6e410
Aleksey Smolyaninov <s.32546875148@gmail.com>
2022-10-09 11:36:44 +0300

    final

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -7,6 +7,6 @@ public class Cli {
         Scanner sc = new Scanner(System.in);
         System.out.println("May I have your name?");
         String name = sc.nextLine();
-        System.out.println("Привет, " + name + "!");
+        System.out.println("Hello, " + name + "!");
     }
 }
